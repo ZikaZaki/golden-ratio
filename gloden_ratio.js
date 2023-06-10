4a4d78d79e3acdaa9c25d42b7d385465542d3170
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-11 02:00:37 +0300

    Update gloden_ratio.js

--- gloden_ratio.js
+++ gloden_ratio.js
@@ -1,1 +1,24 @@
+function checkGoldenRatio(x,y) {
+  const goldenRatio = ((1 + Math.sqrt(5)) / 2).toFixed(6); // calculate the golden ratio
+  const result = x > y ? (x / y).toFixed(6) : (y / x).toFixed(6); // divide the input number by the golden ratio
+  if (goldenRatio === result) {
+      return result;
+  }else {
+      return null;
+  }
+}
 
+// console.log("The Squre Root of 5 is: ", Math.sqrt(5));
+// console.log("The Sum of Sqrt(5) + 1 is: ", Math.sqrt(5) + 1);
+
+
+// console.log("The Golden Ration: ",((1 + Math.sqrt(5)) / 2).toFixed(6));
+function getOtherNum(num) {
+    const phi = (1 + Math.sqrt(5)) / 2;
+    
+    return phi * num;
+}
+
+console.log("\n", getOtherNum(10));
+
+console.log(checkGoldenRatio(10, 16.18033988749895));
